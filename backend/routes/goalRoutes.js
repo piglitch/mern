d94e1ec74b7fe3c771b88e7cf8ffebfbb4cc6d66
piglitch <73684837+piglitch@users.noreply.github.com>
2023-07-22 22:04:17 +0530

    /

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const { getGoals, setGoals, updateGoals, deleteGoals } = require('../controllers/goalController')
 
 
-router.route('/:id').get(getGoals).post(setGoals)
+router.route('/').get(getGoals).post(setGoals)
 router.route('/:id').put(updateGoals).delete(deleteGoals)
 
 module.exports = router
